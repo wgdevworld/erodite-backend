ae0666aa248d1dce2d120ac76708a22d33a838f9
Adam Jin <jinwoonggyu@gmail.com>
2025-09-05 15:09:21 -0400

    feat: add user setting database

---
+++ prisma/migrations/20250905190614_add_user_setting/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "UserSetting" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "UserSetting_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSetting_userId_key" ON "UserSetting"("userId");
+
+-- AddForeignKey
+ALTER TABLE "UserSetting" ADD CONSTRAINT "UserSetting_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/migrations/20250905190857_add_notification_permission/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "UserSetting" ADD COLUMN     "notificationPermission" INTEGER NOT NULL DEFAULT -1;
